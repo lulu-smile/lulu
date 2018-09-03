c30619b8471a68c26e589d6d7651132df3e9fb1f
lulu-smile <2910975998@qq.com>
2018-09-03 18:47:46 +0800

    lunbowenti

--- it's my project/js/slideshow.js
+++ it's my project/js/slideshow.js
@@ -9,13 +9,31 @@
 		else
 			imgShow.parentNode.parentNode.parentNode.children[0].children[0].children[0].className="show";
 	}
-	var timer=setInterval(slide,3000);
-	var slider=document.getElementById("rotate");
+	var timer=setInterval(slide,1000);
+	var slider=document.getElementById("promoInner");
 	slider.onmouseover=function(){
 		clearInterval(timer);
 		timer=null;
 	}
 	slider.onmouseout=function(){
-		timer=setInterval(slide,3000);
+		timer=setInterval(slide,1000);
 	}
+
+	$("#promoTrigger>span>a").hover(function(){
+		console.log(timer)
+		clearInterval(timer);
+		timer=null;
+		var $btn=$(this);
+		var id=$btn.attr("href");
+		$(id).children().addClass("show").parent().parent().siblings().children().children().removeClass("show");
+	},function(){
+		timer=setInterval(slide,1000);
+		console.log(timer)
+	})
+	//Ó¢ÐÛ Æ¤·ô Çø
+	$("div.skin_center>.item_sub>a").hover(function(){
+		var $btn=$(this);
+		var id=$btn.attr("href");
+		$(id).show().siblings().hide();
+	})
 })();
